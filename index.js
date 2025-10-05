0c1a668e76962170f87fe6eea167d4959cbabca3
prosany <mahabubsany2@gmail.com>
2025-10-05 23:48:57 +0600

    Increase request body size limit for JSON and URL-encoded data

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ const Database = require('./libs/initDB');
 const app = express();
 
 app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 // Initialize Database
 const db = new Database({
